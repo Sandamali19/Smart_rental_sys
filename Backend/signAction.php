3c62bcce5e882d950f744b1289b5bc05861f6ee8
Sandamali19 <msandamali41@gmail.com>
2025-10-16 05:15:52 +0530

    rename file paht.because singup.html change to signup.html

--- Backend/signAction.php
+++ Backend/signAction.php
@@ -102,7 +102,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $conn->close();
 
 } else {
-    header("location:../Frontend/singup.html");
+    header("location:../Frontend/signup.html");
     exit;
 }
 ?>
