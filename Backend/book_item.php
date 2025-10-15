40aad6e460948062715b0594739bd7f9f7f52b41
Kalpani-Bandara <kalpaniprarthana29@gmail.com>
2025-10-15 18:10:11 +0530

    create variables for my new HTML page to use

--- Backend/book_item.php
+++ Backend/book_item.php
@@ -13,4 +13,13 @@ $item = $result->fetch_assoc();
 if (!$item) {
     die("Item not found.");
 }
+
+// create variables for my new HTML page to use
+$price_per_day = $item['price'];
+$item_name = htmlspecialchars($item['item_name']);
+$image_path = htmlspecialchars($item['image_path']);
+$description = nl2br(htmlspecialchars($item['description']));
+$location = htmlspecialchars($item['location']);
+
+
 ?>
