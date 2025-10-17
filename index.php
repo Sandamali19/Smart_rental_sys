8ef3598d21402076fbe653f5e6d5436344fb7120
keshini630 <priyanthip844@gmail.com>
2025-10-17 20:34:47 +0530

    fixed index.css path correctly

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RentHub - Rent Anything, Anytime</title>
-    <link rel="stylesheet" href="../Style/index.css">
+    <link rel="stylesheet" href="Style/index.css">
     
     
 </head>
